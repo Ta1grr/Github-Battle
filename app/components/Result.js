a27bca34baf95823435221fc460ebb0497c8016e
Ta1grr <taile602@gmail.com>
2021-01-25 14:57:28 -0600

    latest commit of 1/25/21

--- app/components/Result.js
+++ app/components/Result.js
@@ -1,11 +1,54 @@
 import React from 'react'
+import { battle } from '../utils/api'
 
 export default class Results extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            winner: null,
+            loser: null,
+            error: null,
+            loading: true
+        }
+    }
+
+
+    componentDidMount() {
+        const { playerOne, playerTwo } = this.props
+
+        battle([ playerOne, playerTwo ])
+            .then(players => {
+                this.setState({
+                    winner: players[0],
+                    loser: players[1],
+                    error: null,
+                })
+            }).catch(({message}) => {
+                this.setState({
+                    error: message,
+                    loading: false
+                })
+            })
+    }
+
     render() {
+        const { winner, loser, error, loading } = this.state;
+
+        if (loading === true) {
+            return <p>LOADING</p>
+        }
+
+        if (error) {
+            return (
+                <p className="center-text error"></p>
+            )
+        }
+
         return (
-            <div>
-                Results
-                <pre>{JSON.stringify(this.props, null, 2)}</pre>
+            <div className='grid space-around container-sm'>
+                <div className='card bg-light'></div>
+                <div className='card bg-light'></div>
             </div>
         )
     }
