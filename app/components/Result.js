6d89b85774c95662cbb9e15e0bbd3254e92473f4
Ta1grr <taile602@gmail.com>
2021-02-26 10:34:17 -0600

    added in reusable component

--- app/components/Result.js
+++ app/components/Result.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { battle } from '../utils/api'
 import { FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser } from 'react-icons/fa'
-
+import Card from './Card'
 
 export default class Results extends React.Component {
     constructor(props) {
@@ -50,24 +50,15 @@ export default class Results extends React.Component {
 
         return (
             <div className='grid space-around container-sm'>
-                {`Winner`}
-                <div className='card bg-light'>
-                    <h4 className='header-lg center-text'>
-                        {winner.score === loser.score ? 'Tie' : 'Winner'}
-                    </h4>
-                    <img
-                        className='avatar'
-                        src={winner.profile.avatar_url}
-                        alt={`Avatar for ${winner.profile.login}`}
-                    />
-                    <h4 className='center-text'>
-                        Score: {winner.score.toLocaleString()}
-                    </h4>
-                    <h2 className='center-text'>
-                        <a className='link' href={winner.profile.html_url}>
-                            {winner.profile.login}
-                        </a>
-                    </h2>
+                <Card 
+                    header={winner.score === loser.score ? 'Tie' : 'Winner'}
+                    subheader={`Score: ${winner.score.toLocaleString()}`}
+                    avatar={winner.profile.avatar_url}
+                    href={winner.profile.html_url}
+                    name={winner.profile.login}
+                >
+
+
                     <ul className='card-list'>
                         <li>
                             <FaUser color='rgb(239, 115, 115)' size={22} />
@@ -94,25 +85,14 @@ export default class Results extends React.Component {
                             {winner.profile.following.toLocaleString()} following
                         </li>
                     </ul>
-                </div>
-                {`LOSER`}
-                <div className='card bg-light'>
-                    <h4 className='header-lg center-text'>
-                        {winner.score === loser.score ? 'Tie' : 'Loser'}
-                    </h4>
-                    <img
-                        className='avatar'
-                        src={loser.profile.avatar_url}
-                        alt={`Avatar for ${loser.profile.login}`}
-                    />
-                    <h4 className='center-text'>
-                        Score: {loser.score.toLocaleString()}
-                    </h4>
-                    <h2 className='center-text'>
-                        <a className='link' href={loser.profile.html_url}>
-                            {loser.profile.login}
-                        </a>
-                    </h2>
+                </Card>
+                <Card
+                    header = {winner.score === loser.score ? 'Tie' : 'Loser'}
+                    subheader = {`Score: ${loser.score.toLocaleString()}`}
+                    avatar = {loser.profile.avatar_url}
+                    name = {loser.profile.login}
+                    href = {loser.profile.html_url}
+                >
                     <ul className='card-list'>
                         <li>
                             <FaUser color='rgb(239, 115, 115)' size={22} />
@@ -124,7 +104,7 @@ export default class Results extends React.Component {
                                 {loser.profile.location}
                             </li>
                         )}
-                         {loser.profile.company && (
+                        {loser.profile.company && (
                             <li>
                                 <FaBriefcase color='#795548' size={22} />
                                 {loser.profile.company}
@@ -139,7 +119,7 @@ export default class Results extends React.Component {
                             {loser.profile.following.toLocaleString()} following
                         </li>
                     </ul>
-                </div>
+                </Card>
             </div>
         )
     }
